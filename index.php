d9c8d2179513aeacbb23c5f15e6d865db9bde307
tsuchiya227 <63704190+tsuchiya227@users.noreply.github.com>
2020-04-15 18:45:01 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>PHPバージョン</title>
+    <link rel="stylesheet" href="stylesheet.css">
+  </head>
+  <body>
+    <header>
+
+    </header>
+    <main>
+    <?php
+    $name="つちや";
+    echo <h1>$name</h1>;
+     ?>
+    </main>
+    <footer>
+
+    </footer>
+
+  </body>
+</html>
